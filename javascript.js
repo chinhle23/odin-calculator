3abe1188ceb67562e3361b021d296657bdb5f58a
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-03-03 05:41:55 -0500

    Create operate function

--- javascript.js
+++ javascript.js
@@ -7,3 +7,17 @@ let firstNum
 let secondNum
 let operator
 
+const operate = (a, b, op) => {
+  switch (op) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
+
+console.log(operate(8, 9, '/'));
