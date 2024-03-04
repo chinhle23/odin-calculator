ec96fb2bd5733798a9e55741d6243bb37d2bd2ff
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-03-04 05:14:42 -0500

    Add event listeners for numbers and operators buttons

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@ const subtract = (a, b) => +a - +b;
 const multiply = (a, b) => +a * +b;
 const divide = (a, b) => +a / +b;
 
-let firstNum
-let secondNum
-let operator
+let firstNum = '';
+let secondNum = '';
+let operator = '';
 
 const operate = (a, b, op) => {
   switch (op) {
@@ -20,4 +20,27 @@ const operate = (a, b, op) => {
   }
 }
 
-console.log(operate(8, 9, '/'));
+let display = document.querySelector('#display');
+
+let numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+    if (operator === '') {
+      firstNum += number.value;
+      display.textContent = firstNum;
+    } else {
+      secondNum += number.value;
+      display.textContent = secondNum;
+    }
+  });
+});
+
+let operators = document.querySelectorAll('.operator');
+
+operators.forEach(item => {
+  item.addEventListener('click', () => {
+    operator = item.value;
+    display.textContent = operator;
+  });
+});
